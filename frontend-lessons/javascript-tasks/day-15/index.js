74c94565b24c1d5fe537e60d78432b8c52245936
Chezter Reblando <144419275+urameshiiii@users.noreply.github.com>
2024-01-10 22:45:38 +0800

    Update index.js

--- frontend-lessons/javascript-tasks/day-15/index.js
+++ frontend-lessons/javascript-tasks/day-15/index.js
@@ -1,7 +1,18 @@
 // JavaScript Fundamentals - Day 15: Assignment
 
 // 1. Use a `for` loop to count from 1 to 5 and print each number to the console.
+for(let num=1; num<=5; num++){
+    console.log(num);
+}
 // 2. Create an array named `cookies` with different types of cookies (strings).
+const cookies=["chocolate", "oatmeal", "macaron", "sugar", "biscotti"]
 // 3. Use a `for` loop to iterate over the `cookies` array and print each type of cookie to the console.
+for (let i=0; i<cookies.length; i++){
+    console.log("Eating a "+ cookies[i]+ " cookie.");
+}
 // 4.  Create a variable named `jarNotEmpty` and set it to `true`.
+let jarNotEmpty=true;
 // 5.  Use a `while` loop to simulate eating cookies while the jar is not empty.
+while (jarNotEmpty=true){
+    console.log("Eat a cookie");
+}
