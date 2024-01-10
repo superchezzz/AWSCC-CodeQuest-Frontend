a5486ba55579b2459cfb433caccff5ba2c149ffa
Chezter Reblando <144419275+urameshiiii@users.noreply.github.com>
2024-01-10 21:33:44 +0800

    Update index.js

--- frontend-lessons/javascript-tasks/day-13/index.js
+++ frontend-lessons/javascript-tasks/day-13/index.js
@@ -4,18 +4,38 @@
 
 // 1. Declare a variable named `currentLesson` and assign it the value 'JavaScript Fundamentals'.
 // 2. Print the value of `currentLesson` to the console.
+let currentLesson = 'JavaScript Fundamentals';
+console.log(currentLesson);
 
 // Task 2: Data Types Exploration
 
 // 3. Create a variable named `favoriteNumber` and assign it a number of your choice.
+let favoriteNumber = 8;
 // 4. Create a variable named `favoriteWord` and assign it a string of your choice.
+let favoriteWord ='Pogi';
 // 5. Create a variable named `isJavaScriptFun` and assign it a boolean value.
+let isJavaScriptFun= true;
 // 6. Create a variable named `nothing` and assign it a value of `null`.
+let nothing= null;
 // 7. Create a variable named `undefinedVar` without assigning any value to it.
+let undefinedVar=;
 
 // Task 3: Logical Operators in Action
 
 // 8. Create two variables, `isMorning` and `isRaining`, and assign them boolean values.
+isMorning=true;
+isRaining=false;
 // 9. Use the logical **AND** (`&&`) operator to check if it's morning and raining. Print appropriate messages based on the result.
+if (isMorning&&isRaining){
+    console.log("May pasok, makikita mo si crushiecakes!")
+}else{
+    console.log("Walang pasok hehe")
+}
 // 10. Use the logical **OR** (`||`) operator to check if it's either morning or raining. Print appropriate messages based on the result.
+if (isMorning||isRaining){
+    console.log("Let's code and chill ")
+}else{
+    console.log("Ih?")
+}
 // 11. Use the logical **NOT** (`!`) operator to invert the value of `isMorning`. Print the new value.
+console.log(!isMorning)
