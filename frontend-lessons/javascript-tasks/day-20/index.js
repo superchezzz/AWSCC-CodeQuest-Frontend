2329454f1034fe746f7fa8fb541bb1291a2ff002
Chezter Reblando <clc.reblando@gmail.com>
2024-03-29 02:05:16 +0800

    day-20 js

--- frontend-lessons/javascript-tasks/day-20/index.js
+++ frontend-lessons/javascript-tasks/day-20/index.js
@@ -29,19 +29,30 @@ myHeader.style.backgroundColor="yellow";
 // Task 3: Creating and Appending Elements
 
 // 5. Create a new `h3` element with the text "New Section" using `document.createElement`.
+const h3Element= document.createElement('h3');
+h3Element.textContent="New Section";
 // 6. Append the newly created `h3` element to the end of the body of the HTML document.
-
+document.body.appendChild(h3Element);
 // Task 4: Removing Elements
 
 // 7. Remove the first paragraph from the document.
-
+const firstPara= document.querySelector('p')
+firstPara.remove();
 // Task 5: Handling Events
 
 // 8. Add a click event listener to the div with the ID "header" that logs "Header clicked!" to the console when clicked.
+const header= document.getElementById('header');
 
+header.addEventListener('click', function() {
+    console.log('Header clicked!');
+});
 // Task 6: Advanced Element Manipulation
 
 // 9. Create an array of colors.
+const colors=['red', 'green', 'blue', 'yellow', 'pink'];
 // 10. Use a loop to iterate over all paragraphs and assign a different background color from the array to each paragraph.
-
+const para= document.querySelectorAll('p');
+for(let i=0; i<para.length; i++){
+    para[i].style.backgroundColor= colors[i];
+}
 // *These tasks will help you practice selecting, modifying, creating, and removing elements from the DOM using JavaScript. Good luck! 🌐*
